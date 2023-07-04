85c64206d60c1bed64a4d55fdd5666509ba25084
kate <kat4011yandex@mail.ru>
2023-07-04 21:34:30 +0300

    Home work2.1

--- lesson2Java/dz_2.java
+++ lesson2Java/dz_2.java
@@ -6,7 +6,7 @@
 // Если значение null, то параметр не должен попадать в запрос.
 
 // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
-// ===============================================================================================
+
 
 package lesson2Java;
 import java.util.LinkedHashMap;
@@ -40,5 +40,63 @@ public class dz_2 {
             result.delete(result.length() - 5, result.length());
 
         return result.toString();
+    }
+}
+
+// Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+
+
+package lesson2Java;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class dz_2 {
+    private static File log;
+    private static FileWriter fileWriter;
+
+    public static void main(String[] args) {
+        try {
+            log = new File("log.txt");
+            log.createNewFile();
+            fileWriter = new FileWriter(log);
+            int[] sortArr = { 22, 55, 0, 10, -3, 8 };
+            bubbleSort(sortArr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void bubbleSort(int[] sortArr){
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for(int j = 0; j < sortArr.length - i - 1; j++) {
+                if(sortArr[j + 1] < sortArr[j]) {
+                    int swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
+                }
+            }
+            writeLog(Arrays.toString(sortArr));
+        }
+    }    
+
+    public static void writeLog(String note) {
+        
+        try(FileWriter writer = new FileWriter("log.txt"))
+        {
+           fileWriter.write(note + "\n"); 
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
