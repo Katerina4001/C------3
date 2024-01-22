ba24a4760eb619c07ad9bbab42d80bb1d40b5cf4
kate <kat4011yandex@mail.ru>
2024-01-22 11:15:58 +0300

    Home Work3

---
+++ ООР/lesson1/lesson3/StreamService.java
@@ -0,0 +1,16 @@
+package group4386.gerasimava;
+
+import java.util.List;
+
+public class StreamService {
+    private List<Stream> listStreams;
+
+    public StreamService(List<Stream> listStreams) {
+        this.listStreams = listStreams;
+    }
+
+    public void sortStudentsStreamBySize(){
+        listStreams.sort(new StreamComparator());
+    }
+    
+}
